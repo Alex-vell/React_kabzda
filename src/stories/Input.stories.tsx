45091d2f5fc0b45e4f7f52d2ea5c777436fbfc7e
Alex-vell <https://github.com/Alex-vell/Git-My-first-project.git>
2021-09-13 22:44:08 +0300

    12

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -10,8 +10,8 @@ const Template: Story = (args) => <input {...args} />;
 
 export const UncontrolledInput = Template.bind({});
 
-export const ControlledInput = Template.bind({});
-ControlledInput.args = {
+export const ControlledInputWithFixedValue = Template.bind({});
+ControlledInputWithFixedValue.args = {
     value: 'it-incubator.by'
 }
 
@@ -34,4 +34,32 @@ export const GetValueOfUncontrolledInputByPress: Story = () => {
     return <><input ref={inputRef}/>
         <button onClick={save}>add</button>
         - actual value: {value} </>
+}
+
+
+export const ControlledInput: Story = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+export const ControlledCheckbox: Story = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.currentTarget.checked)
+    }
+    return <input type='checkbox' checked={parentValue} onChange={onChangeHandler}/>
+}
+export const ControlledSelect: Story = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('1')
+    const onChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(event.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChangeHandler}>
+        <option>none</option>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Moscow</option>
+        <option value={'3'}>Kiev</option>
+    </select>
 }
